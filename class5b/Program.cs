d860608b77f8ef7030aac15038e4afbcdd0d0574
mingwang168 <mingwang0904@gmail.com>
2019-11-21 10:29:31 -0800

    Add project files.

---
+++ class5b/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace class5b
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int[] intArray = new int[] { 1, 2, 3 };
+
+            Console.WriteLine(x);
+            Console.WriteLine();
+            foreach (int y in intArray)
+            {
+                Console.WriteLine(y);
+            }
+            Console.WriteLine("---------------------------------------------------------------------");
+
+            AddOne(x);
+            AddOne(intArray);
+
+            Console.WriteLine(x);
+            foreach (int y in intArray)
+            {
+                Console.WriteLine(y);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void AddOne(int x)
+        {
+            x++;
+        }
+
+        static void AddOne(int[] intArray)
+        {
+            for (int x = 0; x < intArray.Length; x++)
+            {
+                intArray[x]++;
+            }
+        }
+
+    }
+}
